367eea41148d4a1889b7663addc45310dea0478f
liyongliang3 <liyongliang3@ceair.com>
2019-08-26 15:26:44 +0800

    v4、v7包迁移到androidx 天剑沉浸式状态栏库、列表顶部悬浮库
    Signed-off-by: liyongliang3 <liyongliang3@ceair.com>

--- app/src/main/java/com/example/smaboy/layouthelper/view/QuickIndexView.java
+++ app/src/main/java/com/example/smaboy/layouthelper/view/QuickIndexView.java
@@ -1,12 +1,11 @@
 package com.example.smaboy.layouthelper.view;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.MainThread;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
