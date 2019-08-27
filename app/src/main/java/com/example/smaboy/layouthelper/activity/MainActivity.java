86821d03af8a32ea0f88e0aef8e510c92e0f7700
liyongliang3 <liyongliang3@ceair.com>
2019-08-27 16:40:13 +0800

    增加新的item
    Signed-off-by: liyongliang3 <liyongliang3@ceair.com>

--- app/src/main/java/com/example/smaboy/layouthelper/activity/MainActivity.java
+++ app/src/main/java/com/example/smaboy/layouthelper/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smaboy.layouthelper.R;
 import com.example.smaboy.layouthelper.base.BaseActivity;
@@ -29,6 +30,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private Button menu;
     private Button custom_photo;
     private Button h5_page;
+    private Button material_page;
+    private Button skeleton;
 
 
 
@@ -47,6 +50,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         menu = findViewById(R.id.menu);
         custom_photo = findViewById(R.id.custom_photo);
         h5_page = findViewById(R.id.h5_page);
+        material_page = findViewById(R.id.material_page);
+        skeleton = findViewById(R.id.skeleton);
 
         flow.setOnClickListener(this);
         quick.setOnClickListener(this);
@@ -54,6 +59,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         menu.setOnClickListener(this);
         custom_photo.setOnClickListener(this);
         h5_page.setOnClickListener(this);
+        material_page.setOnClickListener(this);
+        skeleton.setOnClickListener(this);
     }
 
     @Override
@@ -76,6 +83,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 startActivity(new Intent(MainActivity.this, QuickActivity.class));
                 break;
             case R.id.stick:
+                Toast.makeText(MainActivity.this, "我是粘性头部", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.menu:
                 startActivity(new Intent(MainActivity.this, DataActivity.class));
@@ -83,11 +91,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.skeleton://骨架屏
 
+                Toast.makeText(MainActivity.this, "我是骨架屏", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.custom_photo://自定义拍照
                 startActivity(new Intent(MainActivity.this, CameraActivity.class));
                 break;
             case R.id.h5_page://h5页面
+
+                Toast.makeText(MainActivity.this, "我是h5页面", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.material_page://material页面
+                Toast.makeText(MainActivity.this, "我是material页面", Toast.LENGTH_SHORT).show();
 
                 break;
         }
