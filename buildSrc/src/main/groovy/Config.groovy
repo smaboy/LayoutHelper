23e14f419429609357e5a58e0da21d2f3058dbfc
liyongliang3 <liyongliang3@ceair.com>
2020-01-01 12:49:50 +0800

    增加配置类

---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,64 @@
+class Config {
+
+    /**
+     * 应用id
+     */
+    static applicationId = "com.smaboy.example"
+
+    /**
+     * app名称
+     */
+    static appName = "SmaboyDemo"
+
+    /**
+     * 编译sdk版本
+     */
+    static compileSdkVersion = 29
+
+    /**
+     * 构建工具版本
+     */
+    static buildToolsVersion = '28.0.3'
+
+    /**
+     * 最小sdk版本
+     */
+    static minSdkVersion = 17
+
+    /**
+     * 目标sdk版本
+     */
+    static targetSdkVersion = 29
+
+    /**
+     * 版本代码
+     */
+    static versionCode = 1
+
+    /**
+     * 版本名称
+     */
+    static versionName = '1.0.0'
+
+//    /**
+//     * 第三方库配置
+//     */
+//    static depConfig = [
+//            support      : [
+//                    appcompat_androidx   : "androidx.appcompat:appcompat:${appcompat_androidx_version}",
+//                    recyclerview_androidx: "androidx.recyclerview:recyclerview:$recyclerview_androidx_version",
+//                    design               : "com.google.android.material:material:$design_version",
+//                    multidex             : "com.android.support:multidex:$multidex_version",
+//                    constraint           : "com.android.support.constraint:constraint-layout:$constraint_version",
+//            ],
+//            kotlin       : "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version",
+//            leakcanary   : [
+//                    android         : "com.squareup.leakcanary:leakcanary-android:$leakcanary_version",
+//                    android_no_op   : "com.squareup.leakcanary:leakcanary-android-no-op:$leakcanary_version",
+//                    support_fragment: "com.squareup.leakcanary:leakcanary-support-fragment:$leakcanary_version",
+//            ],
+//    ]
+//
+//    def appcompat_androidx_version = '1.1.1'
+
+}
